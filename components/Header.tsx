377f68c36a41694a9064767a3db0b8d21028da0e
Geraldine <dinegerald10@gmail.com>
2022-08-29 20:48:15 +0200

    Building the Search Bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Image from "next/image";
+import { SearchIcon } from '@heroicons/react/solid'
+
 
 function Header() {
   return (
-    <header className='sticky top-0 z-50'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 '>
 
         {/* Left */}
         <div className='relative flex items-center h-10 cursor-pointer my-auto'>
@@ -13,9 +15,10 @@ function Header() {
             />
         </div>
 
-        {/* Middle */}
-        <div>
-
+        {/* Middle - Search*/}
+        <div className='flex items-center border-2 rounded-full py-2'>
+          <input type="text" placeholder='Start your search' />
+          <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer'/>
         </div>
 
         {/* Right */}
