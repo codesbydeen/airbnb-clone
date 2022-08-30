b88fbfadc884efda7678c4ccb755f4393ebce049
Geraldine <dinegerald10@gmail.com>
2022-08-30 09:39:44 +0200

    Building the Banner Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
@@ -10,11 +11,12 @@ const Home: NextPage = () => {
         <title>CodesByDine Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
+      {/* Header */}
      <Header></Header>
      
      {/* Banner */}
-
+     <Banner />
     </div>
   )
 }
