7363891b62b88416c7b3f8987ee32e77a33d6811
Geraldine <dinegerald10@gmail.com>
2022-09-02 09:06:12 +0200

    smallcard

--- components/SmallCard.tsx
+++ components/SmallCard.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { Key } from 'react'
 import Image from 'next/image';
 
-function Smallcard({ img, location, distance }) {
+function Smallcard({img, location, distance}) {
   return (
-    <div>
+    <div className='flex items-center'>
         {/* Left */}
         <div className='relative h-16 w-16'>
             <Image src={img} layout='fill' className='rounded-lg'/>
         </div>
 
         {/* Right */}
+        <div>
+          <h2>{location}</h2>
+          <h3>{distance}</h3>
+        </div>
     </div>
   )
 }
